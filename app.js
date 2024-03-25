948b82c7b44cf7f97f4c1eff518325a8d143ab44
Sahil1694 <sahilkhilari16@gmail.com>
2024-03-25 11:32:29 +0530

    Added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express"
 import {config} from "dotenv"
 import ErrorMiddleware from "./middlewares/Error.js"
 import cookieParser from "cookie-parser";
+import cors from "cors"
+ 
 config({
     path: "./config/config.env",
 });
@@ -12,7 +14,14 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true, 
 }));
+
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials : true,
+        methods: ["GET","POST","PUT","DELETE"]
+    }))
 
 //Importing & using Routes
 import course from "./routes/courseRoutes.js"
